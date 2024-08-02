c2bee2fc690fb2551a5aa38d8790888ea0ece7ae
ZahimFarooqi0 <160785412+ZahimFarooqi0@users.noreply.github.com>
2024-08-02 17:28:47 +0500

    Common changes

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
-import inquirer from "inquirer";
-import chalk from "chalk";
-console.log(("\n \t\t\t") + chalk.bold.underline.bgRed.black("Welcome To Number-Guessing-Game"));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+const chalk_1 = __importDefault(require("chalk"));
+console.log(("\n \t\t\t") + chalk_1.default.bold.underline.bgRed.black("Welcome To Number-Guessing-Game"));
 console.log("\n");
 let randomnumber = Math.floor(Math.random() * 10 + 1);
-const answer = await inquirer.prompt([{ message: "Kindly Enter Your Guessed Number (1-10) : ", type: "number", name: "userguessednumber" }]);
+const answer = await inquirer_1.default.prompt([{ message: "Kindly Enter Your Guessed Number (1-10) : ", type: "number", name: "userguessednumber" }]);
 if (answer.userguessednumber === randomnumber) {
-    console.log(("\n") + chalk.bgBlue.italic.red("Congraluations You Won !!!"));
+    console.log(("\n") + chalk_1.default.bgBlue.italic.red("Congraluations You Won !!!"));
 }
 else {
-    console.log(("\n") + chalk.bgBlue.italic.red("Better Luck Next Time"))
+    console.log(("\n") + chalk_1.default.bgBlue.italic.red("Better Luck Next Time"));
 }
